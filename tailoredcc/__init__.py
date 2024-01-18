5215105c010358f2c346ce4240c423358d0d705d
Maximilian Scheurer <maximilian.scheurer@covestro.com>
2024-01-18 16:07:13 +0100

    Bump version: 0.0.1 → 0.0.2

--- tailoredcc/__init__.py
+++ tailoredcc/__init__.py
@@ -2,7 +2,7 @@
 # Covestro Deutschland AG, 2023
 from .tailoredcc import ec_cc_from_ci, solve_tccsd_oe, tccsd_from_ci, tccsd_from_fqe
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = [
     "ec_cc_from_ci",
