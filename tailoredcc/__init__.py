66e5ce2d472c4cb2a5a6241783c00b0c9a6eb00a
Maximilian Scheurer <maximilian.scheurer@covestro.com>
2024-01-18 16:03:08 +0100

    really switch

--- tailoredcc/__init__.py
+++ tailoredcc/__init__.py
@@ -1,6 +1,5 @@
 # Proprietary and Confidential
 # Covestro Deutschland AG, 2023
-from ._version import __version__
 from .tailoredcc import ec_cc_from_ci, solve_tccsd_oe, tccsd_from_ci, tccsd_from_fqe
 
 __version__ = "0.0.1"
